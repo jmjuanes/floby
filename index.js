3b846c9dad34dc6edee82012120e0ee6c2dfe766
jmjuanes <josemijuanes@gmail.com>
2017-10-16 19:26:51 +0200

    index.js: exports temporal file manager

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ module.exports = require('./lib/file.js');
 
 //Exports the json file manager
 module.exports.json = require('./lib/json.js');
+
+//Exports the temporal file manager
+module.exports.temp = require('./lib/temp.js');
