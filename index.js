50feca2abfa6b3cefde9b695a36631c25257fb9f
jmjuanes <josemijuanes@gmail.com>
2017-10-17 13:30:32 +0200

    index.js: removed temp object

--- index.js
+++ index.js
@@ -3,6 +3,3 @@ module.exports = require('./lib/file.js');
 
 //Exports the json file manager
 module.exports.json = require('./lib/json.js');
-
-//Exports the temporal file manager
-module.exports.temp = require('./lib/temp.js');
