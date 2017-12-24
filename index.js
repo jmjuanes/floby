a322341497cd04fb52da622b7f6da9e8d8758b0b
jmjuanes <josemijuanes@gmail.com>
2017-12-24 17:38:04 +0100

    index.js: export ini file manager

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ module.exports = require('./lib/file.js');
 
 //Exports the json file manager
 module.exports.json = require('./lib/json.js');
+
+//Exports the ini file manager
+module.exports.ini = require('./lib/ini.js');
